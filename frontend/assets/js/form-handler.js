e6a76865da140454f8ebce28533a3c43b9949ace
Michael <michael@Michaels-MacBook-Pro.local>
2025-02-25 20:57:43 -0500

    implemented 50 second delay to avoid issues

--- frontend/assets/js/form-handler.js
+++ frontend/assets/js/form-handler.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     headers: {
                         "Content-Type": "application/json"
                     },
-                    body: JSON.stringify(jsonData),
+                    body: formData,
                 });
 
                 if (response.ok) {
